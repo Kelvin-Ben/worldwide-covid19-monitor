9f88c2ef54b1c5b090ace9cd221f68e22470e867
Kelvin Ben <kevoben14@gmail.com>
2022-11-25 02:07:04 +0300

    Create components such as Country.jsx for the app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  <div className="py-5 flex justify-center items-center footer-hero fixed bottom-0 w-full">
+    <h1 className="font-thin text-gray-100 tracking-wide text-xs md:text-lg">
+      &copy; Classic 2023. All rights reserved. developed by
+      {' '}
+      <a
+        className="text-white font-bold"
+        href="https://github.com/Kelvin-Ben"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Kelvin Benjamin
+      </a>
+      {' '}
+    </h1>
+  </div>;
+};
+
+export default Footer;
